405dba70f2eeab87630e2d8739aedf8ea61a975a
Max Mathys <max.mathys@gmail.com>
2022-04-08 08:44:36 +0000

    changes

--- sketchedsgd/example.py
+++ sketchedsgd/example.py
@@ -5,7 +5,8 @@ from sketched_optimizer import SketchedSGD, SketchedSum, SketchedModel
 torch.manual_seed(42)
 
 X = torch.randn(1000, 200)
-y = torch.randn(1000, 1)
+#y = torch.randn(1000, 1)
+y = torch.zeros(1000, 1)
 
 model = torch.nn.Sequential(torch.nn.Linear(200, 1))
 model = SketchedModel(model)
@@ -15,7 +16,7 @@ opt = SketchedSGD(opt, k=20, accumulateError=True, p1=0, p2=4)
 
 summer = SketchedSum(opt, c=20, r=5, numWorkers=4)
 
-for i in range(20):
+for i in range(100):
     opt.zero_grad()
 
     yPred = model(X)
